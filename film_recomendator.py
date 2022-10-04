6afc3ec647988e4e6d43bed8799e75de0fca876e
Pablitgithub <102721989+Pablitgithub@users.noreply.github.com>
2022-10-04 18:00:03 +0200

    Delete film_recomendator.py

--- film_recomendator.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-numeraciones = pd.read_csv(
-    'CALLEJERO_VIGENTE_NUMERACIONES_202209.csv', sep=';', encoding='latin-1')
-trameros = pd.read_csv(
-    'CALLEJERO_VIGENTE_TRAMERO_202209.csv', sep=';', encoding='latin-1')
-cruces = pd.read_csv('CALLEJERO_VIGENTE_CRUCES_202209.csv',
-                     sep=';', encoding='latin-1')
-viales = pd.read_csv('CALLEJERO_VIGENTE_VIALES_202209.csv',
-                     sep=';', encoding='latin-1')
