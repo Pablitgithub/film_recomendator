433c69d4429ab1f0ed54eab87c8714fa1b31342a
pablodiazvega <102721989+Pablitgithub@users.noreply.github.com>
2022-11-16 11:19:57 +0100

    Update film_recomendator_regex.py

--- film_recomendator_regex.py
+++ film_recomendator_regex.py
@@ -12,7 +12,11 @@ def extract():
 
 def transform(df):
     entrada = input('Qué género te gustaría ver ')
-    df['genre_contains'] = df['Genre'].apply(lambda x: re.findall(entrada, x))
+    
+    # Crea una nueva columna en las que pondrá  solamentelos géneros que coincidan con la entrada
+    df['genre_contains'] = df['Genre'].apply(lambda x: re.findall(entrada, x))  
+    
+    # Coge en las que encuentre el género en la nueva columna que serán solo aquellas que contengan el género
     data = df[df['genre_contains'].str.len() > 0]
     data = data['Series_Title'].sample()
 
